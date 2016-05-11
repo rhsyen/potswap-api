fcbb43b1b4ec5892cfb0f322360649a0174ba50f
Roderick Yen <rhsyen@gmail.com>
2016-05-11 22:56:59 +1200

    bcrypt password encryption
    Uses bcrypt to encrypt passwords

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
 var Schema = mongoose.Schema;
 
 function setPassword(val) {
-    return val + "_processed";
+    var hash = bcrypt.hashSync(val, saltRounds);
+    return hash;
 }
 
 var UserSchema = new Schema({
